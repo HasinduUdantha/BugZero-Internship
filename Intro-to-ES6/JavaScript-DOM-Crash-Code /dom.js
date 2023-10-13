2c1f2f9c24bcb538cfc5c5d49352466f11a8d42c
HasinduUdantha <hasinduudantha21@gmail.com>
2023-10-13 20:07:12 +0530

    modified: dom.js

--- Intro-to-ES6/JavaScript-DOM-Crash-Code /dom.js
+++ Intro-to-ES6/JavaScript-DOM-Crash-Code /dom.js
@@ -17,37 +17,37 @@
 
 // console.log(document.images);
 
-// GET ELEMENT BY ID //
+// //GET ELEMENT BY ID //
 
-//console.log(document.getElementById("header-title"));
+// console.log(document.getElementById("header-title"));
 
-//var headerTitle = document.getElementById("header-title");
-//var header = document.getElementById("main-header");
-//console.log(headerTitle);
-//headerTitle.textContent = "Hello";
-//headerTitle.innerText = "Goodbye";
-//console.log(headerTitle.innerText);
-//headerTitle.innerHTML = "<h3>Hello</h3>";
+// var headerTitle = document.getElementById("header-title");
+// var header = document.getElementById("main-header");
+// console.log(headerTitle);
+// headerTitle.textContent = "Hello";
+// headerTitle.innerText = "Goodbye";
+// console.log(headerTitle.innerText);
+// headerTitle.innerHTML = "<h3>Hello</h3>";
 
-//header.style.borderBottom = "solid 3px #000";
+// header.style.borderBottom = "solid 3px #000";
 
-// GET ELEMENTS BY CLASSNAME //
+// //GET ELEMENTS BY CLASSNAME //
 
-//var items = document.getElementsByClassName("list-group-item");
+// var items = document.getElementsByClassName("list-group-item");
 // console.log(items);
 // console.log(items[1]);
 // items[1].textContent = "Hello 2"
 // items[1].style.fontWeight = "bold";
 // items[1].style.backgroundColor = "yellow";
 
-//Gives error
-//items.style.backgroundColor = "#f4f4f4";
+// //Gives error
+// items.style.backgroundColor = "#f4f4f4";
 
 // for(var i = 0; i < items.length; i++){
 //     items[i].style.backgroundColor = "#f4f4f4";
 // }
 
-// GET ELEMENTS BY TAG NAME //
+// //GET ELEMENTS BY TAG NAME //
 // var li = document.getElementsByTagName("li");
 
 // console.log(li);
@@ -64,7 +64,7 @@
 // }
 
 
-// QUERY SELECTER //
+// //QUERY SELECTER //
  
 // var header = document.querySelector("#main-header");
 // header.style.borderBottom = "solid 4px #ccc";
@@ -83,7 +83,7 @@
 // var secondItem = document.querySelector(".list-group-item:nth-child(2)");
 // secondItem.style.color = "coral";
 
-// QUERY SELECTOR ALL //
+// //QUERY SELECTOR ALL //
 
 // var titles = document.querySelectorAll(".title");
 
@@ -98,3 +98,81 @@
 //     even[i].style.backgroundColor = "#ccc";
 // }
 
+// // TRAVERSING THE DOM //
+
+//var itemList = document.querySelector("#items");
+// // parentNode
+
+// console.log(itemList.parentNode);
+// itemList.parentNode.style.backgroundColor = "#f4f4f4";
+// console.log(itemList.parentNode.parentNode.parentNode);
+
+// // parentElement
+
+// console.log(itemList.parentElement);
+// itemList.parentElement.style.backgroundColor = "#f4f4f4";
+// console.log(itemList.parentElement.parentElement.parentElement);
+
+// // childNode
+
+//console.log(itemList.childNodes);
+
+// console.log(itemList.children);
+// console.assert.log(itemList.children[1]);
+// itemList.children[1].style.backgroundColor = "yellow";
+
+// // FirstChild 
+// console.log(itemList.firstChild);
+
+// firstElementChild
+// console.log(itemList.firstElementChild);
+// itemList.firstElementChild.textContent = "Hello 1";
+
+// lastElementChild
+// console.log(itemList.lastElementChild);
+// itemList.lastElementChild.textContent = "Hello 4";
+
+// nextSibling
+// console.log(itemList.nextSibling);
+// // nextElementSibling
+// console.log(itemList.nextElementSibling);
+
+// previousSibling
+// console.log(itemList.previousSibling);
+// // previousElementSibling
+// console.log(itemList.previousElementSibling);
+// itemList.previousElementSibling.style.color = "green";
+
+//create Element
+
+// Create a div
+// var newDiv = document.createElement("div");
+
+// // Add class
+// newDiv.className = "hello";
+
+// // Add attr
+// newDiv.setAttribute("title", "Hello Div");
+
+// //create TEXT NODE
+// var newDivText = document.createTextNode("Hello World");
+
+// // Add text to div
+// newDiv.appendChild(newDivText);
+
+// var container = document.querySelector("header .container");
+// var h1 = document.querySelector("header h1");
+
+// console.log(newDiv);
+
+// newDiv.style.fontSize = "30px";
+
+// container.insertBefore(newDiv, h1);
+
+var button = document.getElementById("button").addEventListener("click", buttonClick)
+    
+
+function buttonClick(){
+    console.log("button clicked");
+
+}
