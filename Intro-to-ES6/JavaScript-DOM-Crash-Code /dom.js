163fef7c4e7c4bbd9bc7f1accb52b064b9d6664d
HasinduUdantha <hasinduudantha21@gmail.com>
2023-10-14 22:32:12 +0530

    new file:   Intro to ReactJS/index.html	modified: Intro-toES6/JavaScript-DOM-Crash-Code /dom.js

--- Intro-to-ES6/JavaScript-DOM-Crash-Code /dom.js
+++ Intro-to-ES6/JavaScript-DOM-Crash-Code /dom.js
@@ -2,53 +2,52 @@
 
 // //console.dir(document);
 
-// console.log(document.domain);
-// console.log(document.URL);
-// console.log(document.title);
-// //document.title = 123;
-// console.log(document.doctype);
-// console.log(document.head);
-// console.log(document.body);
-// console.log(document.all);
-// console.log(document.all[11]);
-// // document.all[10].textContent = "Hello";
-// console.log(document.forms[0]);
-// console.log(document.links);
+ console.log(document.domain);
+ console.log(document.URL);
+ console.log(document.title);
+ //document.title = 123;
+  console.log(document.doctype);
+ console.log(document.head);
+ console.log(document.body);
+ console.log(document.all);
+ console.log(document.all[11]);
+ // document.all[10].textContent = "Hello";
+ console.log(document.forms[0]);
+ console.log(document.links);
 
-// console.log(document.images);
+ console.log(document.images);
 
-// //GET ELEMENT BY ID //
+ //GET ELEMENT BY ID //
 
-// console.log(document.getElementById("header-title"));
+ console.log(document.getElementById("header-title"));
 
-// var headerTitle = document.getElementById("header-title");
-// var header = document.getElementById("main-header");
-// console.log(headerTitle);
-// headerTitle.textContent = "Hello";
-// headerTitle.innerText = "Goodbye";
-// console.log(headerTitle.innerText);
-// headerTitle.innerHTML = "<h3>Hello</h3>";
+ var headerTitle = document.getElementById("header-title");
+ var header = document.getElementById("main-header");
+ console.log(headerTitle);
+ headerTitle.textContent = "Hello";
+ headerTitle.innerText = "Goodbye";
+ console.log(headerTitle.innerText);
+ headerTitle.innerHTML = "<h3>Hello</h3>";
 
-// header.style.borderBottom = "solid 3px #000";
+ header.style.borderBottom = "solid 3px #000";
 
-// //GET ELEMENTS BY CLASSNAME //
+ //GET ELEMENTS BY CLASSNAME //
 
-// var items = document.getElementsByClassName("list-group-item");
-// console.log(items);
-// console.log(items[1]);
-// items[1].textContent = "Hello 2"
-// items[1].style.fontWeight = "bold";
-// items[1].style.backgroundColor = "yellow";
+ var items = document.getElementsByClassName("list-group-item");
+ console.log(items);
+ console.log(items[1]);
+ items[1].textContent = "Hello 2"
+ items[1].style.fontWeight = "bold";
+ items[1].style.backgroundColor = "yellow";
+ //Gives error
+ items.style.backgroundColor = "#f4f4f4";
 
-// //Gives error
-// items.style.backgroundColor = "#f4f4f4";
+ for(var i = 0; i < items.length; i++){
+     items[i].style.backgroundColor = "#f4f4f4";
+ }
 
-// for(var i = 0; i < items.length; i++){
-//     items[i].style.backgroundColor = "#f4f4f4";
-// }
-
-// //GET ELEMENTS BY TAG NAME //
-// var li = document.getElementsByTagName("li");
+ //GET ELEMENTS BY TAG NAME //
+ var li = document.getElementsByTagName("li");
 
 // console.log(li);
 // console.log(li[1]);
