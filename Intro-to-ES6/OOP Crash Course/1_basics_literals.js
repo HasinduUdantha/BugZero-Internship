622a256f9430d8d68de1efefbd9e355f27030e50
HasinduUdantha <hasinduudantha21@gmail.com>
2023-10-23 11:10:46 +0530

    create: prototypes.js

--- Intro-to-ES6/OOP Crash Course/1_basics_literals.js
+++ Intro-to-ES6/OOP Crash Course/1_basics_literals.js
@@ -10,23 +10,23 @@
 
 
 // // Object Literal
-const book1 = {
-    title: 'Book One',
-    author: 'John Doe',
-    year: '2023',
-    getSummery: function(){
-        return `${this.title} was written by ${this.author} in ${this.year}`;
-    }
-}
+// const book1 = {
+//     title: 'Book One',
+//     author: 'John Doe',
+//     year: '2023',
+//     getSummery: function(){
+//         return `${this.title} was written by ${this.author} in ${this.year}`;
+//     }
+// }
 
-const book2 = {
-    title: 'Book Two',
-    author: 'Jane Doe',
-    year: '2016',
-    getSummery: function(){
-        return `${this.title} was written by ${this.author} in ${this.year}`;
-    }
-}
+// const book2 = {
+//     title: 'Book Two',
+//     author: 'Jane Doe',
+//     year: '2016',
+//     getSummery: function(){
+//         return `${this.title} was written by ${this.author} in ${this.year}`;
+//     }
+// }
 // console.log(book1.getSummery());
 // console.log(book2.getSummery());
 // console.log(Object.values(book2));
---
+++ Intro-to-ES6/OOP Crash Course/3_prototypes.js
@@ -0,0 +1,25 @@
+// Constructor
+function Book(title, author, year){
+    this.title = title;
+    this.author = author;
+    this.year = year;
+
+    this.getSummery = function(){
+        return `${this.title} was written by ${this.author} in ${this.year}`;
+    }
+};
+
+// getSummery
+Book.prototype.getSummery = function() {
+    return `${this.title} was written by ${this.author} in ${this.year}`;
+    
+};
+
+
+// Instatiate an Object
+const book1 = new Book("Book One", "John Doe", "2013");
+const book2 = new Book("Book Two", "Jane Doe", "2016");
+
+console.log(book2.getSummery());
+console.log(book1.title);
+
