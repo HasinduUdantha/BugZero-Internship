f246095cfa3ec15b290767dcbbe9d12b73b766e6
HasinduUdantha <hasinduudantha21@gmail.com>
2023-10-23 20:41:59 +0530

    create , modified: OOP Crash Course

--- Intro-to-ES6/OOP Crash Course/4_inheritance.js
+++ Intro-to-ES6/OOP Crash Course/4_inheritance.js
@@ -14,3 +14,22 @@ Book.prototype.getSummery = function() {
     return `${this.title} was written by ${this.author} in ${this.year}`;
 
 };
+
+// // Magazine Constructor
+
+function Magazine(title,author,year,month){
+    Book.call(this,title,author,year);
+
+    this.month = month;
+};
+
+// // Inherit Prototype
+Magazine.prototype = Object.create(Book.prototype);
+
+// // Instantiate Magazine Object
+const mag1 = new Magazine("Mag One", "John Doe", "2018", "Jan");
+
+// // Use Magzine Constructor
+Magazine.prototype.constructor = Magazine;
+
+console.log(mag1);
