6f3d91fa0f4aad4468aa0efa496d9a22750a22ed
HasinduUdantha <hasinduudantha21@gmail.com>
2023-10-24 11:53:41 +0530

    create: AJAX crash course

--- Intro-to-ES6/Async_JavaScript/promises.js
+++ Intro-to-ES6/Async_JavaScript/promises.js
@@ -13,9 +13,43 @@ function getPosts(){
     },1000)
 };
 
-function createPost(post, callback) {
-    setTimeout(() => {
-        posts.push(post);
-        callback();
-    },2000);
+function createPost(post) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            posts.push(post);
+
+            const error = true;
+
+            if(!error){
+                resolve();
+            }else {
+                reject('Error: Something went wrong');
+            }
+        },2000);
+    });
 }
+
+// createPost({ title: 'Post Three', body: 'This is post Three ' })
+// .then(getPosts)
+// .catch(err => console.log(err));
+
+
+// // Async / Await 
+async function init(){
+    createPost({ title: 'Post Three', body: 'This is post Three ' })
+    getPosts();
+}
+
+init();
+// // Promise.all
+
+// const promise1 = Promise.resolve('Hello World');
+// const promise2 = 10;
+// const promise3 = new Promise((resolve, reject) =>
+// setTimeout(resolve, 2000, 'gOODBYE')
+// );
+
+// Promise.all([promise1,promise2,promise3]).then(values => console.log(values));
+
+
+
