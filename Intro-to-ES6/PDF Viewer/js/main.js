b6db527814032374b7fbf4fce70ccebfe547c2cb
HasinduUdantha <hasinduudantha21@gmail.com>
2023-10-25 12:31:21 +0530

    create: state capital search

--- Intro-to-ES6/PDF Viewer/js/main.js
+++ Intro-to-ES6/PDF Viewer/js/main.js
@@ -6,4 +6,26 @@ let pdfDoc = null,
     pageNumIsPending = null;
 
 const scale = 1.5,
+    canvas = document.querySelector('#pdf-render'),
+    ctx = canvas.getContext('2d');
+
+// Render the page
+const renderPage = num => {
+
+    pageIsRendering = true;
+
+    //Get page
+    pdfDoc.getPage(num).then(page => {
+        console.log(page);
+    })
+}   
+
+// Get Document 
+pdfjsLib.getDocument(url).promise.then(pdfDoc_ => {
+
+    pdfDoc = pdfDoc_;
     
+    document.querySelector('#page-count').textContent = pdfDoc.numPages;
+
+    renderPage(pageNum);
+});
