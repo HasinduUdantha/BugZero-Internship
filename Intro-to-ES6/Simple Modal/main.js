2946c464ab999e1b8c425bc4ffc315a69f4aea8b
HasinduUdantha <hasinduudantha21@gmail.com>
2023-10-26 13:21:45 +0530

    modified: main.js

---
+++ Intro-to-ES6/Simple Modal/main.js
@@ -0,0 +1,29 @@
+// Get modal element
+var modal = document.getElementById("simpleModal");
+// Get open modal button
+var modalBtn = document.getElementById("modal-Btn");
+// Get close button
+var closeBtn = document.getElementsByClassName("closeBtn")[0];
+
+// Listen for open click
+modalBtn.addEventListener("click", openModal);
+
+// Listen for close click
+closeBtn.addEventListener("click", closeModal);
+
+// Listen for outside click
+window.addEventListener("click", clickOutside);
+
+function openModal() {
+  modal.style.display = "block";
+}
+
+function closeModal() {
+  modal.style.display = "none";
+}
+
+function clickOutside(e){
+    if(e.target == modal){
+        modal.style.display = 'none'
+    }
+}
