ed30667ccedbc16cd6bb706a774080f7d2799a01
HasinduUdantha <hasinduudantha21@gmail.com>
2023-10-31 13:24:38 +0530

    create: AddTask.js

--- Intro to ReactJS/React-Task-Track/react-task-tracker/src/App.js
+++ Intro to ReactJS/React-Task-Track/react-task-tracker/src/App.js
@@ -24,11 +24,28 @@ function App() {
       reminder: false,
     },
   ]);
+
+  // Delete Task
+  const deleteTask = (id) => {
+    setTasks(tasks.filter((task) => task.id !== id));
+  };
+
+  // Toggle Reminder
+  const toggleReminder = (id) => {
+    setTasks(
+      tasks.map((task) =>
+        task.id === id ? { ...task, reminder: !task.reminder } : task
+      )
+    );
+  };
   return (
     <div className="container">
-      <Header /> 
-      <Button />
-      <Tasks tasks={tasks}/>
+      <Header />
+      {tasks.length > 0 ? (
+        <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />
+      ) : (
+        "No Tasks To Show"
+      )}
     </div>
   );
 }
