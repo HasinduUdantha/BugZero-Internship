ab934826b32b343f8bae7eafbdf226e7ef9fe4ae
HasinduUdantha <hasinduudantha21@gmail.com>
2023-11-02 23:27:58 +0530

    modified: App.js

--- Intro to ReactJS/Dojo-Blog/dojo-blog/src/App.js
+++ Intro to ReactJS/Dojo-Blog/dojo-blog/src/App.js
@@ -3,9 +3,7 @@ import Navbar from "./Navbar";
 import Home from "./Home";
 
 function App() {
-  // const title = "Welcome to the new blog";
-  // const likes = 50;
-  //const person = {name: 'yoshi', age: 30} cannot output objects and boolean values
+  
 const link = "http://www.google.com"
   return (
     <div className="App">
