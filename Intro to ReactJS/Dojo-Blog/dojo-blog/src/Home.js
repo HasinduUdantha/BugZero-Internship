fd234b70fd887372b3b3b552c9fd96bd1d51d999
HasinduUdantha <hasinduudantha21@gmail.com>
2023-11-06 23:16:31 +0530

    Modified: Home.js

--- Intro to ReactJS/Dojo-Blog/dojo-blog/src/Home.js
+++ Intro to ReactJS/Dojo-Blog/dojo-blog/src/Home.js
@@ -4,8 +4,7 @@ const Home = () => {
   const [blogs, setBlogs] = useState([
     { title: "My new website", body: "lorem ipsum...", author: "mario", id: 1 },
     { title: "Welcome party!", body: "lorem ipsum...", author: "yoshi", id: 2 },
-    {
-      title: "Web dev top tips",
+    { title: "Web dev top tips",
       body: "lorem ipsum...",
       author: "mario",
       id: 3,
