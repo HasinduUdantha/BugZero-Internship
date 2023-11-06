8416c0fd1777ec93812809e372e1e4573484d310
HasinduUdantha <hasinduudantha21@gmail.com>
2023-11-06 12:33:50 +0530

    modified: Home.js

--- Intro to ReactJS/Dojo-Blog/dojo-blog/src/Home.js
+++ Intro to ReactJS/Dojo-Blog/dojo-blog/src/Home.js
@@ -1,12 +1,21 @@
 const Home = () => {
+
+    const handleClick = (e) => {
+        console.log("Hello Ninjas", e)
+    }
+
+    const handleClickAgain = (name,e) => {
+        console.log("Hello" + name, e.target);
+    }
     return ( 
         <div className="home">
             <h2>Homepage</h2>
+            <button onClick={handleClick}>Click Me</button>
+            <button onClick={(e) => 
+                handleClickAgain('mario',e)
+            }>Click Me Again</button>
         </div>
      );
 }
  
 export default Home;
-<div className="home">
-    <h2>Homepage</h2>
-</div>
