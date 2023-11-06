a6eeb2dfbf4a18a1379a77c4392099f38408511c
HasinduUdantha <hasinduudantha21@gmail.com>
2023-11-06 22:20:36 +0530

    modified: Home.js

--- Intro to ReactJS/Dojo-Blog/dojo-blog/src/Home.js
+++ Intro to ReactJS/Dojo-Blog/dojo-blog/src/Home.js
@@ -1,22 +1,33 @@
-import { useState } from 'react';
-
+import { useState } from "react";
 
 const Home = () => {
-  //let name = "mario";
-    const [name, setName] = useState('mario')
-    const [age,setAge] = useState(23)
+  const [blogs, setBlogs] = useState([
+    { title: "My new website", body: "lorem ipsum...", author: "mario", id: 1 },
+    { title: "Welcome party!", body: "lorem ipsum...", author: "yoshi", id: 2 },
+    {
+      title: "Web dev top tips",
+      body: "lorem ipsum...",
+      author: "mario",
+      id: 3,
+    },
+  ]);
 
-  const handleClick = () => {
-    setName('luigi'); 
-    setAge('25');
-    
-  };
+  //let name = "mario";
+  //     const [name, setName] = useState('mario')
+  //     const [age,setAge] = useState(23)
+
+  //   const handleClick = () => {
+  //     setName('luigi');
+  //     setAge('25');
+
+  //   };
 
   return (
     <div className="home">
-      <h2>Homepage</h2>
-      <p>{ name } is { age } old </p>
-      <button onClick={handleClick}>Click Me</button>
+      <h2></h2>
+
+      {/* <p>{ name } is { age } old </p>
+      <button onClick={handleClick}>Click Me</button> */}
     </div>
   );
 };
