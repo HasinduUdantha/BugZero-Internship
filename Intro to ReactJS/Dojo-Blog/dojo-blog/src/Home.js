8ed93a20312fa6954808dccbae1dbe9b02f5dec4
HasinduUdantha <hasinduudantha21@gmail.com>
2023-11-06 13:26:24 +0530

    modified: Home.js

--- Intro to ReactJS/Dojo-Blog/dojo-blog/src/Home.js
+++ Intro to ReactJS/Dojo-Blog/dojo-blog/src/Home.js
@@ -1,21 +1,24 @@
+import { useState } from 'react';
+
+
 const Home = () => {
+  //let name = "mario";
+    const [name, setName] = useState('mario')
+    const [age,setAge] = useState(23)
 
-    const handleClick = (e) => {
-        console.log("Hello Ninjas", e)
-    }
+  const handleClick = () => {
+    setName('luigi'); 
+    setAge('25');
+    
+  };
 
-    const handleClickAgain = (name,e) => {
-        console.log("Hello" + name, e.target);
-    }
-    return ( 
-        <div className="home">
-            <h2>Homepage</h2>
-            <button onClick={handleClick}>Click Me</button>
-            <button onClick={(e) => 
-                handleClickAgain('mario',e)
-            }>Click Me Again</button>
-        </div>
-     );
-}
- 
+  return (
+    <div className="home">
+      <h2>Homepage</h2>
+      <p>{ name } is { age } old </p>
+      <button onClick={handleClick}>Click Me</button>
+    </div>
+  );
+};
+
 export default Home;
