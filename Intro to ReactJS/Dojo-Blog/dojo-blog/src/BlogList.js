81c37a6810c017f43c36c3d90ed8b042a750d612
HasinduUdantha <hasinduudantha21@gmail.com>
2023-11-08 11:33:10 +0530

    modified: BlogList.js (react props)

--- Intro to ReactJS/Dojo-Blog/dojo-blog/src/BlogList.js
+++ Intro to ReactJS/Dojo-Blog/dojo-blog/src/BlogList.js
@@ -2,6 +2,8 @@ const BlogList = (props) => {
   const blogs = props.blogs;
   const title = props.title;
 
+  
+
   return (
     <div className="blog-list">
       <h2>{title}</h2>
@@ -9,6 +11,7 @@ const BlogList = (props) => {
         <div className="blog-preview" key={blog.id}>
           <h2>{blog.title}</h2>
           <p>Written by {blog.author}</p>
+          <button onClick={() => handleDelete(blog.id)}>Delete Blog</button>
         </div>
       ))}
     </div>
