43eccacfd51414702c84b74647eb58c16c8adae9
HasinduUdantha <hasinduudantha21@gmail.com>
2023-11-08 11:40:30 +0530

    modified: BlogList.js #13 functions as props

--- Intro to ReactJS/Dojo-Blog/dojo-blog/src/BlogList.js
+++ Intro to ReactJS/Dojo-Blog/dojo-blog/src/BlogList.js
@@ -1,9 +1,4 @@
-const BlogList = (props) => {
-  const blogs = props.blogs;
-  const title = props.title;
-
-  
-
+const BlogList = ({ blogs, title, handleDelete }) => {
   return (
     <div className="blog-list">
       <h2>{title}</h2>
