eaec038f43c2d1996dbac0b5c698b531925220b8
HasinduUdantha <hasinduudantha21@gmail.com>
2023-11-08 13:41:56 +0530

    modified: Home.js #18 Conditional Loading Message

--- Intro to ReactJS/Dojo-Blog/dojo-blog/src/Home.js
+++ Intro to ReactJS/Dojo-Blog/dojo-blog/src/Home.js
@@ -35,25 +35,54 @@
 //   );
 // };
 
+// // export default Home;
+// import { useEffect, useState } from "react";
+// import BlogList from "./BlogList";
+
+// const Home = () => {
+//   const [blogs, setBlogs] = useState(null)
+
+//   useEffect(() => {
+//     fetch('http://localhost:8000/blogs')
+//       .then(res => {
+//         return res.json();
+//       })
+//       .then(data => {
+//         setBlogs(data);
+//       })
+//   }, [])
+
+//   return (
+//     <div className="home">
+//       {blogs && <BlogList blogs={blogs} />}
+//     </div>
+//   );
+// }
+ 
 // export default Home;
 import { useEffect, useState } from "react";
 import BlogList from "./BlogList";
 
 const Home = () => {
-  const [blogs, setBlogs] = useState(null)
+  const [blogs, setBlogs] = useState(null);
+  const [isPending, setIsPending] = useState(true);
 
   useEffect(() => {
-    fetch('http://localhost:8000/blogs')
+    setTimeout(() => {
+      fetch('http://localhost:8000/blogs')
       .then(res => {
         return res.json();
       })
       .then(data => {
+        setIsPending(false);
         setBlogs(data);
       })
+    }, 1000);
   }, [])
 
   return (
     <div className="home">
+      { isPending && <div>Loading...</div> }
       {blogs && <BlogList blogs={blogs} />}
     </div>
   );
