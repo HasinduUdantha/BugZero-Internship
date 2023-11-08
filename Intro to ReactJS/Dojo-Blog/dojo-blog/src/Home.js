0e57a3d57d0c669b508689e99724101e1e24f59d
HasinduUdantha <hasinduudantha21@gmail.com>
2023-11-08 12:48:30 +0530

    modified: Home.js #15

--- Intro to ReactJS/Dojo-Blog/dojo-blog/src/Home.js
+++ Intro to ReactJS/Dojo-Blog/dojo-blog/src/Home.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import BlogList from "./BlogList";
 
 const Home = () => {
@@ -13,15 +13,24 @@ const Home = () => {
     },
   ]);
 
+  const [name, setName] = useState("mario");
+
   const handleDelete = (id) => {
-    const newBlogs = blogs.filter(blog => blog.id !== id)
-    setBlogs(newBlogs)
-  }
+    const newBlogs = blogs.filter((blog) => blog.id !== id);
+    setBlogs(newBlogs);
+  };
+
+  useEffect(() => {
+    console.log("use effec run");
+    console.log(name)
+  }, [name]);
 
   return (
     <div className="home">
-      <BlogList blogs={blogs} title="All Blogs!" handleDelete={handleDelete}/>
+      <BlogList blogs={blogs} title="All Blogs!" handleDelete={handleDelete} />
       {/* <BlogList blogs={blogs.filter((blog) => blog.author === 'mario')} title="Mario's Blogs!" /> */}
+      <button onClick={() => setName('luigi')}>change name</button>
+      <p>{name}</p>
     </div>
   );
 };
