afcbb620659ac81a9107cf37151c8d41b0863a1b
HasinduUdantha <hasinduudantha21@gmail.com>
2023-11-09 21:33:07 +0530

    modified: useFetch

--- Intro to ReactJS/Dojo-Blog/dojo-blog/src/App.js
+++ Intro to ReactJS/Dojo-Blog/dojo-blog/src/App.js
@@ -10,10 +10,10 @@ function App() {
         <Navbar />
         <div className="content">
           <Switch>
-            <Route path="/">
+            <Route exact path="/">
               <Home />
             </Route>
-            <Route path="/Create">
+            <Route path="/create">
               <Create />
             </Route>
           </Switch>
