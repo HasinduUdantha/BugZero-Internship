20084f4a6d0fe32c6a5aa3c39ef9838d2e8350a3
HasinduUdantha <hasinduudantha21@gmail.com>
2023-11-15 13:49:54 +0530

    create: react-file-upload app

---
+++ Intro to ReactJS/React File Upload/client/src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="container mt-4">
+      <h4 className="display-4 text-center mb-4">
+      <i class="fab fa-react">React File Upload</i>
+      </h4>
+    </div>
+  );
+}
+
+export default App;
