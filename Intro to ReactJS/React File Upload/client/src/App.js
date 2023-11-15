f95263d8be03bd54afbedcf308d41a3520186e80
HasinduUdantha <hasinduudantha21@gmail.com>
2023-11-15 14:30:24 +0530

    modified: FileUpload.js,Message.js,Progress.js

--- Intro to ReactJS/React File Upload/client/src/App.js
+++ Intro to ReactJS/React File Upload/client/src/App.js
@@ -1,12 +1,14 @@
-
-import './App.css';
+import FileUpload from "./components/FileUpload";
+import "./App.css";
 
 function App() {
   return (
     <div className="container mt-4">
       <h4 className="display-4 text-center mb-4">
-      <i class="fab fa-react">React File Upload</i>
+        <i className="fab fa-react" /> React File Upload
       </h4>
+
+      <FileUpload />
     </div>
   );
 }
