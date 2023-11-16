25c4ae72d8877f457e5e07c68de32e89505699df
HasinduUdantha <hasinduudantha21@gmail.com>
2023-11-16 12:49:17 +0530

    create: Posts.js

--- Intro to ReactJS/React Pagination/pagination/src/App.js
+++ Intro to ReactJS/React Pagination/pagination/src/App.js
@@ -1,14 +1,24 @@
-import {useState, useEffect} from 'react';
-import axios from 'axios';
-import './App.css';
+import { useState, useEffect } from "react";
+import axios from "axios";
+import "./App.css";
 
 function App() {
-
   const [posts, setPosts] = useState([]);
-  const [loading,setLoading] = useState(false);
-  const [currentPage,setCurrentPage] = useState(1)
-  const [postsPerPage,setPostsPerPage] = useState(10);
-  
+  const [loading, setLoading] = useState(false);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [postsPerPage, setPostsPerPage] = useState(10);
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      setLoading(true);
+      const res = await axios.get("https://jsonplaceholder.typicode.com/posts");
+
+      setPosts(res.data);
+      setLoading(false);
+    };
+    fetchPosts();
+  }, []);
+
   return (
     <div className="container">
       <h1>My App</h1>
