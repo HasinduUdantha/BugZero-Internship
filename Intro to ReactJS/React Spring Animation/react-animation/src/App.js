4a19697516669d6b579bec320d4fc4e29b20fbdd
HasinduUdantha <hasinduudantha21@gmail.com>
2023-11-17 12:12:25 +0530

    create: react-animation

---
+++ Intro to ReactJS/React Spring Animation/react-animation/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Component1 from './components/Component1';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Welcome</h1>
+      <Component1 />
+    </div>
+  );
+}
+
+export default App;
---
+++ Intro to ReactJS/React Spring Animation/react-animation/src/components/Component1.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function Component1() {
+  return (
+    <div>
+      <h1>Component 1</h1>
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Omnis
+        voluptatibus natus quam vero asperiores amet laudantium ipsum officia
+        rerum possimus quod, eligendi deleniti fugiat, porro itaque ipsam.
+        Placeat, error dolor.
+      </p>
+    </div>
+  );
+}
