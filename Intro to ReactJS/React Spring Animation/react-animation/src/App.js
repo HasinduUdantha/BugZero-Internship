9d1a36bf1b3323623f14e5efb5c74f4affe783bb
HasinduUdantha <hasinduudantha21@gmail.com>
2023-11-17 12:27:07 +0530

    modified: react-animations

--- Intro to ReactJS/React Spring Animation/react-animation/src/App.js
+++ Intro to ReactJS/React Spring Animation/react-animation/src/App.js
@@ -1,13 +1,37 @@
-import './App.css';
-import Component1 from './components/Component1';
+import "./App.css";
+import ReactDOM from "react-dom";
+import { Transition, animated } from "react-spring";
+
+import Component1 from "./components/Component1";
+import Component2 from "./components/Component2";
+import Component3 from "./components/Component3";
+
+
 
 function App() {
   return (
     <div className="App">
-      <h1>Welcome</h1>
-      <Component1 />
-    </div>
+        <Component1 />
+        <Component2 toggle={this.toggle} />
+        <Transition
+          native
+          items={this.state.showComponent3}
+          from={{ opacity: 0 }}
+          enter={{ opacity: 1 }}
+          leave={{ opacity: 0 }}
+        >
+          {show =>
+            show &&
+            (props => (
+              <animated.div style={props}>
+                <Component3 />
+              </animated.div>
+            ))
+          }
+        </Transition>
+      </div>
   );
 }
+
 
 export default App;
