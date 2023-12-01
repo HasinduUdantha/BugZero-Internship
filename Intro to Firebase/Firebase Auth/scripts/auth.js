13eb88f22032fe36944a5602f53807b2487fae14
HasinduUdantha <hasinduudantha21@gmail.com>
2023-12-01 13:16:45 +0530

    modified: auth.js

--- Intro to Firebase/Firebase Auth/scripts/auth.js
+++ Intro to Firebase/Firebase Auth/scripts/auth.js
@@ -1,16 +1,38 @@
 // listen for auth status changes
-auth.onAuthStateChanged(user => {
-    if (user) {
-      db.collection('guides').get().then(snapshot => {
+auth.onAuthStateChanged((user) => {
+  if (user) {
+    db.collection("guides").onSnapshot(
+      (snapshot) => {
         setupGuides(snapshot.docs);
         setupUI(user);
-      });
-    } else {
-      setupUI();
-      setupGuides([]);
-    }
-  })
-  
+      },
+      (err) => console.log(err.message)
+    );
+  } else {
+    setupUI();
+    setupGuides([]);
+  }
+});
+
+// create new guide
+const createForm = document.querySelector("#create-form");
+createForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  db.collection("guides")
+    .add({
+      title: createForm.title.value,
+      content: createForm.content.value,
+    })
+    .then(() => {
+      // close the create modal & reset form
+      const modal = document.querySelector("#modal-create");
+      M.Modal.getInstance(modal).close();
+      createForm.reset();
+    })
+    .catch((err) => {
+      console.log(err.message);
+    });
+});
 
 // signup
 const signupForm = document.querySelector("#signup-form");
@@ -21,13 +43,20 @@ signupForm.addEventListener("submit", (e) => {
   const email = signupForm["signup-email"].value;
   const password = signupForm["signup-password"].value;
 
-  // sign up the user
-  auth.createUserWithEmailAndPassword(email, password).then((cred) => {
-    // close the signup modal & reset form
-    const modal = document.querySelector("#modal-signup");
-    M.Modal.getInstance(modal).close();
-    signupForm.reset();
-  });
+  // sign up the user & add firestore data
+  auth
+    .createUserWithEmailAndPassword(email, password)
+    .then((cred) => {
+      return db.collection("users").doc(cred.user.uid).set({
+        bio: signupForm["signup-bio"].value,
+      });
+    })
+    .then(() => {
+      // close the signup modal & reset form
+      const modal = document.querySelector("#modal-signup");
+      M.Modal.getInstance(modal).close();
+      signupForm.reset();
+    });
 });
 
 // logout
