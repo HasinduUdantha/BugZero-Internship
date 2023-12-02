a79e07202e0126d10e14f08c74b1a504d920833d
HasinduUdantha <hasinduudantha21@gmail.com>
2023-12-02 23:24:35 +0530

    modified: app.js

--- Intro to Firebase/firebase hosting/public/app.js
+++ Intro to Firebase/firebase hosting/public/app.js
@@ -1,18 +1,17 @@
 document.addEventListener("DOMContentLoaded", (event) => {
   const app = firebase.app();
   const db = firebase.firestore();
-  
-  const productsRef = db.collection('products');
 
-  const query = productsRef.where('price', '>', 10)
+  const productsRef = db.collection("products");
 
-  query.get()
-  .then(products => {
-    products.forEach(doc => {
-        data = doc.data()
-        document.write(`${data.name} at ${data.price} <br>`)
-    })
-  })
+  const query = productsRef.where("price", ">", 10);
+
+  query.get().then((products) => {
+    products.forEach((doc) => {
+      data = doc.data();
+      document.write(`${data.name} at ${data.price} <br>`);
+    });
+  });
 });
 
 // function googleLogin() {
