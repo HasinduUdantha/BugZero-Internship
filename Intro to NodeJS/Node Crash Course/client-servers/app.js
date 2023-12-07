4a89e2f7085444e78464a7af6e8936c9698ab4fc
HasinduUdantha <hasinduudantha21@gmail.com>
2023-12-07 14:16:17 +0530

    modified: index.ejs

--- Intro to NodeJS/Node Crash Course/client-servers/app.js
+++ Intro to NodeJS/Node Crash Course/client-servers/app.js
@@ -9,15 +9,29 @@ app.set("view engine", "ejs");
 app.listen(3000);
 
 app.get("/", (req, res) => {
+  const blogs = [
+    {
+      title: "Yoshi finds eggs",
+      snippet: "Lorem ipsum dolor sit amet consectetur",
+    },
+    {
+      title: "Mario finds stars",
+      snippet: "Lorem ipsum dolor sit amet consectetur",
+    },
+    {
+      title: "How to defeat bowser",
+      snippet: "Lorem ipsum dolor sit amet consectetur",
+    },
+  ];
   //res.send("<p>Home Page </p>");
   //res.sendFile("./views/index.html", { root: __dirname });
-  res.render("index");
+  res.render("index", { title: "Home", blogs });
 });
 
 app.get("/about", (req, res) => {
   //res.send("<p>About Page </p>");
   //res.sendFile("./views/about.html", { root: __dirname });
-  res.render("about");
+  res.render("about", { title: "About" });
 });
 
 // redirects
@@ -27,11 +41,11 @@ app.get("./about-us", (req, res) => {
 });
 
 app.get("./blogs/create", (req, res) => {
-  res.render();
+  res.render("create", { title: "Create a new blog" });
 });
 
 // 404 page
 app.use((req, res) => {
   //res.status(404).sendFile("./views/404.html", { root: __dirname });
-  res.status(404).render("404");
+  res.status(404).render("404", { title: "404" });
 });
