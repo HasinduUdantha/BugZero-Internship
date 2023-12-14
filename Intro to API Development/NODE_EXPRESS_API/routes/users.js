c07af00ab49b0fac801df0e80805272d32d37505
HasinduUdantha <hasinduudantha21@gmail.com>
2023-12-14 22:41:42 +0530

    modified: users.js

--- Intro to API Development/NODE_EXPRESS_API/routes/users.js
+++ Intro to API Development/NODE_EXPRESS_API/routes/users.js
@@ -1,8 +1,30 @@
 import express from "express";
+import { v4 as uuidv4 } from 'uuid';
 
 const router = express.Router();
+const users = [
+  
+];
 // all routes in here are starting with /users
 router.get("/", (req, res) => {
-  res.send("Hello");
+  console.log(users);
+  res.send(users);
 });
+
+router.post("/", (req, res) => {
+  const user = req.body;
+
+  const userId = uuidv4(); 
+
+  
+
+  users.push({ ...user, id: userId});
+
+  res.send(`User with the name ${user.firstName} added to the database!`);
+
+});
+router.get('/:id', (req,res) => {
+    res.send(res)
+})
 export default router;
+  
