162018c9b79c4338a587240fd9292fb4725eedd2
HasinduUdantha <hasinduudantha21@gmail.com>
2023-12-15 09:54:17 +0530

    add: DELETE ,POST method

--- Intro to API Development/NODE_EXPRESS_API/routes/users.js
+++ Intro to API Development/NODE_EXPRESS_API/routes/users.js
@@ -1,10 +1,8 @@
 import express from "express";
-import { v4 as uuidv4 } from 'uuid';
+import { v4 as uuidv4 } from "uuid";
 
 const router = express.Router();
-const users = [
-  
-];
+let users = [];
 // all routes in here are starting with /users
 router.get("/", (req, res) => {
   console.log(users);
@@ -14,17 +12,24 @@ router.get("/", (req, res) => {
 router.post("/", (req, res) => {
   const user = req.body;
 
-  const userId = uuidv4(); 
+  const userId = uuidv4();
 
-  
-
-  users.push({ ...user, id: userId});
+  users.push({ ...user, id: userId });
 
   res.send(`User with the name ${user.firstName} added to the database!`);
-
 });
-router.get('/:id', (req,res) => {
-    res.send(res)
-})
+router.get("/:id", (req, res) => {
+  const { id } = req.params;
+
+  const foundUser = users.find((user) => user.id === id);
+
+  res.send(foundUser);
+});
+
+router.delete("/:id", (req, res) => {
+  const { id } = req.params;
+
+  users = users.filter((user) => user.id !== id);
+  res.send(`User with the id ${id} is deleted from the database.`)
+});
 export default router;
-  
