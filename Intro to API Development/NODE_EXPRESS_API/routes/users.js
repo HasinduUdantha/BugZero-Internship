371b3fb5e0a74d5456dc8ec059de772f143eade3
HasinduUdantha <hasinduudantha21@gmail.com>
2023-12-15 11:27:17 +0530

    add: DELETE and PATCH

--- Intro to API Development/NODE_EXPRESS_API/routes/users.js
+++ Intro to API Development/NODE_EXPRESS_API/routes/users.js
@@ -2,7 +2,9 @@ import express from "express";
 import { v4 as uuidv4 } from "uuid";
 
 const router = express.Router();
+
 let users = [];
+
 // all routes in here are starting with /users
 router.get("/", (req, res) => {
   console.log(users);
@@ -30,6 +32,25 @@ router.delete("/:id", (req, res) => {
   const { id } = req.params;
 
   users = users.filter((user) => user.id !== id);
-  res.send(`User with the id ${id} is deleted from the database.`)
+  res.send(`User with the id ${id} is deleted from the database.`);
+});
+
+router.patch("/:id", (req, res) => {
+  const { id } = req.params;
+  const { firstName, lastName, age } = req.body;
+
+  const user = users.find((user) => user.id === id);
+
+  if (firstName) {
+    user.firstName = firstName;
+  }
+  if (lastName) {
+    user.lastName = lastName;
+  }
+  if (age) {
+    user.age = age;
+  }
+
+  res.send(`User with id ${id} has been updated`);
 });
 export default router;
