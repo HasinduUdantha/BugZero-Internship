c0073666fe4701709fa554ba1fcf9987a46933cf
HasinduUdantha <hasinduudantha21@gmail.com>
2024-01-31 12:34:11 +0530

    modifeid:app.js

---
+++ Intro to ReactJS/Apex React/apex-react/src/components/popChart.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import Chart from "react-apexcharts";
+
+class popChart extends Component {
+  render() {
+    return <h1>Chart</h1>;
+  }
+}
+
+export default popChart;
